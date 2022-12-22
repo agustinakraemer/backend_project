66304797cb86ac70735a43eba7d17850bfd6f44e
Agustina Kraemer <agustinakraemer@gmail.com>
2022-12-22 19:22:31 -0300

    arreglo

--- index.js
+++ index.js
@@ -6,24 +6,30 @@ class ProductManager {
 
     addProduct (title, description, price, thumbnail, code, stock){
         if(!title || !description || !price || !thumbnail || !code || !stock ){
-            console.log("Not found")
+            console.log("Completa todos los campos")
+        } else if (this.productos.includes(code)){
+            console.log('El codigo ya está en uso')
         } else {
             const producto = {
-            id: this.#generarId(),
-            title,
-            description, 
-            price, 
-            thumbnail, 
-            code, 
-            stock,
-        }}
-        this.productos.push(producto);
+                id: this.#generarId(),
+                title,
+                description, 
+                price, 
+                thumbnail, 
+                code, 
+                stock,
+            }
+            this.productos.push(producto);
+        }
+        
     }
     #generarId() {
         let id = this.productos.length === 0 ? 1 : this.productos[this.productos.length - 1].id + 1
         return id;
     }
+    #codigoId (){
 
+    }
     getProducts() {
         return this.productos;
     }
@@ -39,7 +45,10 @@ class ProductManager {
 }
 
 const productManager = new ProductManager()
-productManager.addProduct ('Producto1', '23cm x 28cm', '1500', 'imagen', '435')
+productManager.addProduct ('Producto1', '23cm x 28cm', '1500', 'imagen', '435','30')
+productManager.addProduct ('Producto2', '29cm x 32cm', '200', 'imagen', '435','30')
 console.log(productManager)
+console.log(productManager)
+
 
 
