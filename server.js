6399e3dc02e44ea9c6bae07e87f7207a0b28f279
Agustina Kraemer <agustinakraemer@gmail.com>
2023-01-26 14:29:17 -0300

    modificaciones

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-/* import handlebars from 'express-handlebars' */
+import handlebars from 'express-handlebars' 
 import productsRouter from './src/routes/products.router.js'
 import cartRouter from './src/routes/cart.router.js'
 //dirname
-/* import { dirname } from 'path'
+import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url)) */
+const __dirname = dirname(fileURLToPath(import.meta.url))
 //-------
 const app = express()
 
@@ -15,16 +15,20 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 //handlebars
-/* app.engine('handlebars', handlebars.engine())
+app.engine('handlebars', handlebars.engine())
 app.set('view engine', 'handlebars')
-app.set('views',__dirname+'/views') */
+app.set('views',__dirname+'/views') 
 
-/* app.get('/',(req, res)=>{
+app.get('/',(req, res)=>{
     res.render('vista1')
 })
-app.get('/vista2',(req, res)=>{
+app.get('/productos',(req, res)=>{
+    const productos = 
+    res.render('realTimeProducts',{productos})
+})
+/* app.get('/vista2',(req, res)=>{
     res.render('vista2')
-}) */
+})  */
 
 //Routes
 app.use('/api/products', productsRouter)
